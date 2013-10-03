7f6ae822123104e683fd3f1f95974e032b83f2fc
Andrew Tipton <andrew@kiwidrew.com>
2013-10-03 23:14:06 +0800

    Add a helper function to retrieve a row from the system catalog tables by Oid.

---
+++ catalog_funcs.c
@@ -0,0 +1,81 @@
+/*
+ * Utility functions for looking up information in the system catalogs which
+ * is not provided by the catcache/relcache infrastructure.
+ *
+ * pg_schema_triggers/catalog_funcs.c
+ */
+
+
+#include "postgres.h"
+#include "access/genam.h"
+#include "access/heapam.h"
+#include "access/htup.h"
+#include "access/htup_details.h"
+#include "access/sysattr.h"
+#include "access/xact.h"
+#include "catalog/indexing.h"
+#include "catalog/pg_class.h"
+#include "utils/lsyscache.h"
+
+
+#include "catalog_funcs.h"
+
+
+/*
+ * Fetch a tuple from a system catalog by Oid.  The returned HeapTuple is
+ * suitable for use with HeapTupleGetDatum(), and must be freed by calling
+ * heap_freetuple().
+ */
+HeapTuple
+catalog_fetch_tuple(Oid relation, Oid index, Oid row, Snapshot snapshot)
+{
+    Relation	reldesc;
+    SysScanDesc	relscan;
+    ScanKeyData	key[1];
+    HeapTuple	reltuple;
+    Oid			reltypeid;
+	
+	/* Scan key is an Oid. */
+	ScanKeyInit(&key[0],
+				ObjectIdAttributeNumber,
+				BTEqualStrategyNumber,
+				184,		/* F_OIDEQ (from backend/utils/fmgroids.h) */
+				ObjectIdGetDatum(row));
+
+	/* Get the Oid of the relation's rowtype. */
+	reltypeid = get_rel_type_id(relation);
+	if (reltypeid == InvalidOid)
+		elog(ERROR, "catalog_fetch_tuple:  relation %u has no rowtype", relation);
+
+	/* Open the catalog relation and fetch a tuple using the given index. */
+	reldesc = heap_open(relation, AccessShareLock);
+	relscan = systable_beginscan(reldesc,
+								 index,
+								 true,
+								 snapshot,
+								 1, key);
+	reltuple = systable_getnext(relscan);
+
+	/* Copy the tuple. */
+	if (!HeapTupleIsValid(reltuple))
+		goto finish;
+
+	/* Copy the tuple and ensure that the Datum headers are set. */
+	reltuple = heap_copytuple(reltuple);
+	HeapTupleHeaderSetDatumLength(reltuple->t_data, reltuple->t_len);
+	HeapTupleHeaderSetTypeId(reltuple->t_data, reltypeid);
+	HeapTupleHeaderSetTypMod(reltuple->t_data, -1);
+		
+finish:
+	/* Close the relation and return the copied tuple. */
+	systable_endscan(relscan);
+	heap_close(reldesc, AccessShareLock);
+	return reltuple;
+}
+
+
+HeapTuple
+pgclass_fetch_tuple(Oid row, Snapshot snapshot)
+{
+	return catalog_fetch_tuple(RelationRelationId, ClassOidIndexId, row, snapshot);
+}
---
+++ catalog_funcs.h
@@ -0,0 +1,26 @@
+/*-------------------------------------------------------------------------
+ *
+ * catalog_funcs.h
+ *    Declarations for system catalog access functions.
+ *
+ *
+ * pg_schema_triggers/catalog_funcs.h
+ *
+ *-------------------------------------------------------------------------
+ */
+#ifndef SCHEMA_TRIGGERS_CATALOG_FUNCS_H
+#define SCHEMA_TRIGGERS_CATALOG_FUNCS_H
+
+
+#include "postgres.h"
+#include "access/htup.h"
+#include "utils/snapshot.h"
+
+
+HeapTuple catalog_fetch_tuple(Oid relation, Oid index, Oid row, Snapshot snapshot);
+
+/* Convenience wrappers around catalog_fetch_tuple(). */
+HeapTuple pgclass_fetch_tuple(Oid row, Snapshot snapshot);
+
+
+#endif	/* SCHEMA_TRIGGERS_CATALOG_FUNCS_H */
